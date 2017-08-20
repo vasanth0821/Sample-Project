d4d863ed1ea6bfd98195e118a0f7f75bcfb61033
Ubuntu <ubuntu@ip-172-31-19-137.ec2.internal>
2017-08-20 16:57:42 +0000

    First commist

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
  app.get('/',function(req,res) {
-   res.send("Hello World");
+   res.send("Happy Birthday vasanth");
 });
 
  app.listen(5000);
