0b8a84d4828236c1b40b762e18062854c976f2f3
Ubuntu <ubuntu@ip-172-31-19-137.ec2.internal>
2017-08-20 17:36:22 +0000

    Hello

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
  app.get('/',function(req,res) {
-   res.send("Happy Birthday vasanthhhhh");
+   res.send("Hello World");
 });
 
  app.listen(5000);
